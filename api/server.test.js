78a2bf6af03c1c948844f5b92c8f6409492ca6be
jasonelliots <j.elliotschwartz@gmail.com>
2020-07-23 10:00:33 -0700

    started

---
+++ api/server.test.js
@@ -0,0 +1,34 @@
+const server = require('./server.js');
+const supertest = require('supertest');
+
+describe('server', function() {
+    it('runs the tests', function() {
+        expect(true).toBe(true);
+    });
+
+
+    describe('GET /', function() {
+        it('should respond with 200 OK', function() {
+            return supertest(server)
+                .get("/")
+                .then(res => {
+                    expect(res.status).toBe(200);
+                })
+        });
+
+        it("should respond with JSON", function () {
+            return supertest(server)
+                .get("/")
+                .then(res => {
+                    expect(res.type).toMatch(/json/i);
+                });
+        });
+        it("should respond with api: 'up'", function () {
+            return supertest(server)
+                .get("/")
+                .then(res => {
+                    expect(res.body.api).toBe("up");
+                });
+        });
+    })
+})
